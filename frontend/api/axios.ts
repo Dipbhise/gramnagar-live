8ec8a8a6a0d75974c2a16346d8134f16422c013c
Dipak Digambar Bhise <dipakbhise154@gmail.com>
2026-06-28 23:45:49 +0530

    baseurl change

--- frontend/api/axios.ts
+++ frontend/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { STORAGE_KEYS } from '../constants';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://gramnagar-live.onrender.com',
 });
 
 // Add a request interceptor to include the token in all requests
